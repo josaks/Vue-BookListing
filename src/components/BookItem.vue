6b1bbf639f399e7a08f92c7bd7d32f7a8443eecd
josaks <josaks@online.no>
2018-10-09 12:26:47 +0200

    Create bookitem component

---
+++ src/components/BookItem.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <li>{{ book.title }}:{{ book.author }}</li>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BookItem',
+    props: ['books'],
+}
+</script>
+
+<style>
+    li {
+        display: block;
+        margin: 0 10px;
+    }
+</style>
